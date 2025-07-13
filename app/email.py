5b8e064dbec8d6ab154bc34e3a9299da35a58f31
zeeshan <zeeshanrabnawaz9@gmail.com>
2025-07-13 14:10:58 +0500

    fixed config.py

--- app/email.py
+++ app/email.py
@@ -5,7 +5,7 @@ from . import mail
 
 def send_email(to, subject, template, **kwargs):
 
-    msg = Message(subject, recipients=[to], sender='zeeshanrabnawaz9')
+    msg = Message(subject, recipients=[to], sender='username')
 
     msg.body = render_template(f"{template}.txt", **kwargs)
     msg.html = render_template(f"{template}.html", **kwargs)
--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'zeeshanrabnawaz9@gmail.com'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'zeeshanrabnawaz9@gmail.com'
+    FLASKY_MAIL_SENDER = 'email_address'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'admin_email_address'
 
     @staticmethod
     def init_app(app):
@@ -21,8 +21,8 @@ class DevelopmentConfig(Config):
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'zeeshanrabnawaz9'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'mvse cwli rmtp itsg'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'testusername'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'abcd efgh ijkl mnop'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(base_dir, 'data-dev.sqlite')
     FLASKY_COMMENTS_PER_PAGE = 5
